b86bbfdf03b8695b105e3586a67312ee683fcfc1
sedx876 <swatkins876@gmail.com>
2020-10-08 08:39:59 -0400

    index.js setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Client from 'shopify-buy';
+import { Provider } from 'react-redux';
+import store from './store';
+import './styles/shopify.css';
 
+// build shopify client
+const client = Client.buildClient({
+    storefrontAccessToken: 'YOUR_SHOPIFY_STOREFRONT_ACCESS_TOKEN',
+    domain: 'YOUR_MYSHOPIFY_STORE_URL'
+});
+store.dispatch({type: 'CLIENT_CREATED', payload: client});
+
+// buildClient() is synchronous, so we can call all these after!
+client.product.fetchAll().then((res) => {
+  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
+});
+client.checkout.create().then((res) => {
+  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
+});
+client.shop.fetchInfo().then((res) => {
+  store.dispatch({type: 'SHOP_FOUND', payload: res});
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,  
   document.getElementById('root')
 );
 
