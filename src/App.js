ed0aded5c33fd517eb454b90eb95e4f7db86ac75
sedx876 <swatkins876@gmail.com>
2020-10-08 08:52:03 -0400

    work on store.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './styles/App.css';
+import Cart from './components/shopify/Cart';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.handleCartOpen = this.handleCartOpen.bind(this);
+  }
+
+  render(){
   return (
     <div className="App">
       
     </div>
   );
 }
-
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import Client from 'shopify-buy';
 import { Provider } from 'react-redux';
 import store from './store';
 import './styles/shopify.css';
 
-// build shopify client
-const client = Client.buildClient({
-    storefrontAccessToken: 'YOUR_SHOPIFY_STOREFRONT_ACCESS_TOKEN',
-    domain: 'YOUR_MYSHOPIFY_STORE_URL'
-});
-store.dispatch({type: 'CLIENT_CREATED', payload: client});
-
-// buildClient() is synchronous, so we can call all these after!
-client.product.fetchAll().then((res) => {
-  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
-});
-client.checkout.create().then((res) => {
-  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
-});
-client.shop.fetchInfo().then((res) => {
-  store.dispatch({type: 'SHOP_FOUND', payload: res});
-});
+const client = Client.buildClient({   
+  storefrontAccessToken: 'your-access-token',    
+  domain: 'your-shopify-url.myshopify.com'});
 
 ReactDOM.render(
   <Provider store={store}>
